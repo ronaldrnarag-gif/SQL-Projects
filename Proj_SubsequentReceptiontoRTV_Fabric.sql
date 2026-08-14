1c6e83e0e3aa721dd2b38c5fae5ff4daeb9e73e4
ronaldrnarag-gif <ronaldr.narag@gmail.com>
2026-08-14 16:44:21 +0400

    Create vw_fnl_repurchasereport and USD costs
    Convert script into a CREATE VIEW (vw_fnl_repurchasereport). Join dimexchangeratedwh to convert purchase/return costs to USD (costpurchased_usd / costreturns_usd) and adjust aggregates accordingly. Add year/month columns, change final source from repurchase_agg to repurchase_data, and rename output flags to isrepurchase / issameqty. Also scope date/purchasetype predicates with table aliases and remove the top-level USE statement.

--- Proj_SubsequentReceptiontoRTV_Fabric.sql
+++ Proj_SubsequentReceptiontoRTV_Fabric.sql
@@ -1,6 +1,6 @@
-USE AzadeaWarehouse
-			
--- 		
+
+
+		
 
 /*
 Title       : Report on Rtv's with subsequent Rcp
@@ -17,18 +17,25 @@ Logic       :
 Stakeholder : Internal Control
 Created     : ronaldn/20260814
 */
-			
-; with purchase_agg as (			
+
+-- DROP VIEW if exists vw_fnl_repurchasereport 
+
+CREATE View vw_fnl_repurchasereport as
+
+
+with purchase_agg as (			
         select a.date, a.companyid, a.productkey, b.productid, a.ltitemgroupid_it itemgroupid, a.apntprimaryvendorid_it vendorid, c.vendorname,			
                 b.hir1 Dept, b.hir2 SDept, b.hir3 Cls, b.hir4 Scls,			
-                sum(a.qtypurchased) qtypurchased, sum(a.costpurchased) costpurchased			
+                sum(a.qtypurchased) qtypurchased, sum(a.costpurchased * d.exchangeratenew) costpurchased_usd			
         from factpurchase a			
         left join dimproduct b			
                 on a.productkey=b.productkey and upper(a.companyid)=upper(b.companyid)	
         left join dimvendor c
                 on a.apntprimaryvendorid_it = c.vendorid
-        where date between '2026-02-01' and getdate()-1			
-        and purchasetype = 'Purchase Order'			
+        left join dimexchangeratedwh d
+                on upper(a.companyid) = upper(d.companyid)
+        where a.date between '2026-02-01' and getdate()-1			
+        and a.purchasetype = 'Purchase Order'			
         group by a.date, a.companyid, a.productkey, b.productid, a.ltitemgroupid_it, a.apntprimaryvendorid_it, c.vendorname,			
                 b.hir1 , b.hir2 , b.hir3 , b.hir4 			
         ),		
@@ -36,14 +43,16 @@ Created     : ronaldn/20260814
 returns_agg as (			
         select a.date, a.companyid, a.productkey, b.productid, a.ltitemgroupid_it itemgroupid, a.apntprimaryvendorid_it vendorid, c.vendorname,			
                 b.hir1 Dept, b.hir2 SDept, b.hir3 Cls, b.hir4 Scls,			
-                sum(a.qtypurchased) qtyreturns, sum(a.costpurchased) costreturns			
+                sum(a.qtypurchased) qtyreturns, sum(a.costpurchased * d.exchangeratenew) costreturns_usd			
         from factpurchase a			
         left join dimproduct b			
                 on a.productkey=b.productkey and upper(a.companyid)=upper(b.companyid)	
         left join dimvendor c
                 on a.apntprimaryvendorid_it = c.vendorid
-        where date between '2026-02-01' and getdate()-1			
-        and purchasetype = 'Return Order'			
+        left join dimexchangeratedwh d
+                on upper(a.companyid) = upper(d.companyid)
+        where a.date between '2026-02-01' and getdate()-1			
+        and a.purchasetype = 'Return Order'			
         group by a.date, a.companyid, a.productkey, b.productid, a.ltitemgroupid_it, a.apntprimaryvendorid_it, c.vendorname,			
                 b.hir1 , b.hir2 , b.hir3 , b.hir4 			
         ),
@@ -51,8 +60,7 @@ returns_agg as (
 repurchase_agg as (
         select a.date, a.companyid, a.productkey, a.productid, a.itemgroupid, a.vendorid, a.vendorname,			
                         a.Dept, a.SDept, a.Cls, a.Scls,			
-                sum(a.qtyreturns) qtyreturns, sum(a.costreturns) costreturns,			
-                isnull(sum(b.qtypurchased),0) qtypurchased, isnull(sum(b.costpurchased),0) costpurchased
+                isnull(sum(b.qtypurchased),0) qtypurchased, isnull(sum(b.costpurchased_usd),0) costpurchased_usd
         from returns_agg a			
         left join purchase_agg b			
                 on a.companyid=b.companyid			
@@ -60,16 +68,30 @@ repurchase_agg as (
                 and a.date <= b.date and b.date <= dateadd(day,15,a.date)			
         group by a.date, a.companyid, a.productkey, a.productid, a.itemgroupid, a.vendorid, a.vendorname,			
                         a.Dept, a.SDept, a.Cls, a.Scls					
-        )
+        ),
 
-select *,
+repurchase_data as (
+        select a.date, a.companyid, a.productkey, a.productid, a.itemgroupid, a.vendorid, a.vendorname,			
+                        a.Dept, a.SDept, a.Cls, a.Scls,	
+                        sum(a.qtyreturns) qtyreturns, sum(a.costreturns_usd) costreturns_usd,
+                        sum(b.qtypurchased) qtypurchased, sum(costpurchased_usd) costpurchased_usd
+        from returns_agg a
+        left join repurchase_agg b
+            on a.companyid=b.companyid			
+            and a.productkey=b.productkey	
+            and a.date=b.date
+        group by a.date, a.companyid, a.productkey, a.productid, a.itemgroupid, a.vendorid, a.vendorname,			
+                        a.Dept, a.SDept, a.Cls, a.Scls
+    )
+
+select year(date) year, month(date) month,*,
     case when qtypurchased > 0 
         and -qtyreturns > 2
         then 'Y' else 'N'
-    end as 'repurchaseyn',
+    end as 'isrepurchase',
     case when -qtyreturns = qtypurchased 
         then 'Y' else 'N'
-    end as 'sameqtyrepurchaseyn'
-from repurchase_agg
+    end as 'issameqty'
+from repurchase_data
 where itemgroupid in ('N','I','C')
 
