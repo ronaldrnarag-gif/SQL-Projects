8a983a489b59154b9222d353aec3fd70235d0170
ronaldrnarag-gif <ronaldr.narag@gmail.com>
2026-08-16 11:02:29 +0400

    Update SQLQueryExercises - 12 - Fabric readiness.sql

--- SQLQueryExercises - 12 - Fabric readiness.sql
+++ SQLQueryExercises - 12 - Fabric readiness.sql
@@ -24,9 +24,37 @@ SELECT * FROM INFORMATION_SCHEMA.VIEWS
 
 select * from sys.dm_exec_connections
 
-select * from sys.dm_exec_sessions where nt_user_name = 'ronald.narag'
+-- Which users are connected, Which machines (host_name) they are connecting from, Which application is connecting (program_name), How long they've been connected
+select * from sys.dm_exec_sessions 
 
-select * from sys.dm_exec_requests
+-- Identify Long-Running Queries via 'total_elapsed_time' expressed in milliseconds
+select (total_elapsed_time/1000)/60 ElapsedinMins, *
+from sys.dm_exec_requests 
 
 
+-- Find Blocked sessions
+SELECT
+    session_id,
+    blocking_session_id,
+    wait_type,
+    wait_time
+FROM sys.dm_exec_requests
+WHERE blocking_session_id <> 0;
 
+-- CPU Hungry Queries
+SELECT
+    session_id,
+    cpu_time,
+    total_elapsed_time,
+    command
+FROM sys.dm_exec_requests
+ORDER BY cpu_time DESC;
+
+-- Monitor Query Progress
+-- ETL completion %, Backup/restore progress, Index rebuild progress
+SELECT
+    session_id,
+    command,
+    percent_complete
+FROM sys.dm_exec_requests
+WHERE percent_complete > 0;
